cfb418512687366031d837da87ea6e2be11f7be2
chenshaopeng <shaopeng.chen@mcttechnology.com>
2021-10-22 18:01:13 +0800

    fixed

--- src/app/home/renderPage/first/first.component.ts
+++ src/app/home/renderPage/first/first.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Service } from '../service/reader.api.service';
 import { RenderService, RenderPage } from '../service/render-page.service';
-
+import {
+  DxValidationGroupComponent,
+  DxFormComponent } from 'devextreme-angular';
 @Component({
   providers: [Service],
   selector: 'app-first',
@@ -9,6 +11,12 @@ import { RenderService, RenderPage } from '../service/render-page.service';
   styleUrls: ['./first.component.scss']
 })
 export class FirstComponent implements OnInit {
+  @ViewChild('form', { static: false })
+  form: DxFormComponent;
+  @ViewChild('form2', { static: false })
+  form2: DxFormComponent;
+  @ViewChild('validGroup', { static: false }) validGroup: DxValidationGroupComponent;
+  @ViewChildren(DxFormComponent) dxForms: QueryList<DxFormComponent>;
   employee: any = {};
   positions: any = []
   rules = { 'X': /[02-9]/ };
@@ -18,6 +26,22 @@ export class FirstComponent implements OnInit {
   ) { 
     this.positions = servicedata.getPositions();
   }
+
+  submit = async () => {
+    if (this.validate()) {
+      console.log(1)
+    }
+  }
+  validate = () => {
+      const formList = this.dxForms.toArray();
+      let isValid = true;
+      formList.forEach(item => {
+        if (item.instance.validate().isValid === false) {
+          isValid = false;
+        }
+      });
+      return isValid;
+  };
 
   ngOnInit(): void {
   }
