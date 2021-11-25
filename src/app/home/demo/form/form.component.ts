59c085081fe1eef401edecc1b1193d54168200c6
chenshaopeng <shaopeng.chen@mcttechnology.com>
2021-11-25 18:56:11 +0800

    cehsi

--- src/app/home/demo/form/form.component.ts
+++ src/app/home/demo/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DxScrollViewComponent, DxTextAreaComponent } from 'devextreme-angular';
 
 @Component({
   selector: 'app-form',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  @ViewChild(DxTextAreaComponent, { static: false }) textBox: DxTextAreaComponent;
+  @ViewChild(DxScrollViewComponent, { static: false }) scrollView: DxScrollViewComponent;
+
   formData: any = {}
   items=[
     {
@@ -34,6 +38,28 @@ export class FormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  onKeyDown(e: any) {
+    console.log(e)
+    if (e.event.which == 13) {
+      e.event.preventDefault();
+  }
+    // e.event.preventDefault();
+    // e.event.stopPropagation();
+  }
+  ngAfterViewInit() {
+    const taht = this
+    setTimeout(() => {
+      taht.scrollView.instance.scrollTo({
+        left: 0,
+        top: this.scrollView.instance.scrollHeight(),
+      });
+      setTimeout(() => {
+        taht.textBox.instance.focus()
+      }, 3000);
+      
+    }, 3000);
   }
 
 }
