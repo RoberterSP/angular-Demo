3c84617a32d2932feae5158bb242088a7b6bbdf3
chenshaopeng <shaopeng.chen@mcttechnology.com>
2022-01-21 18:53:06 +0800

    add ng-content  插槽功能

--- src/app/home/operation/main/main.component.ts
+++ src/app/home/operation/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 import { AnimalService } from '../animal.service';
 import { FlowerService } from '../flower.service';
 import { LeafService } from '../leaf.service';
@@ -9,6 +10,7 @@ import { LeafService } from '../leaf.service';
   styleUrls: ['./main.component.scss']
 })
 export class OperationMainComponent implements OnInit {
+  arr: any[];
 
   constructor(
     public flower: FlowerService, 
@@ -17,6 +19,27 @@ export class OperationMainComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.arr = this.getAa()
+  }
+
+  click() {
+    const arr = _.cloneDeep(this.arr)
+    arr.push({
+      Id: arr.length,
+      Name: `this is ${arr.length} length`
+    })
+    this.arr = arr
+  }
+
+  trackByItems(index: number, item: any): number { return item.Id; }
+
+  getAa() {
+    return [
+      {
+        Id: 1,
+        Name: 'this is first'
+      }
+    ]
   }
 
 }
--- src/app/home/operation/skepself/skepself.component.ts
+++ src/app/home/operation/skepself/skepself.component.ts
@@ -5,7 +5,7 @@ import { LeafService } from '../leaf.service';
 
 @Component({
   providers: [
-    { provide: LeafService, useValue: { emoji: '🍁' } },
+    { provide: LeafService, useValue: { emoji: '🍁', extend: 'this is extend' } },
     { provide: FlowerService, useValue: { emoji: '🌻' }},
     { provide: AnimalService, useValue: { emoji: '🐶' } }
   ],
@@ -17,13 +17,15 @@ import { LeafService } from '../leaf.service';
 export class OperationSkepselfComponent implements OnInit {
 // 要在代码中看到这一点，请先假定 emoji 的以下值就是父组件正在使用的值，如本服务所示：
 // 想象一下，在子组件中，你有一个不同的值 🍁（枫叶），但你想使用父项的值。你就要使用 @SkipSelf()
-// </ng-content>  的是如何投影
-  constructor(@Optional() @SkipSelf() public leaf?: LeafService,
+// </ng-content>  的是如何投影  @Optional() @SkipSelf() 
+  constructor(public leaf?: LeafService,
   public flower?: FlowerService,
   @Optional() @SkipSelf() public animal?: AnimalService
   ) { }
 
   ngOnInit(): void {
+    // this.leaf?.emoji = '🍁'
+    // this.leaf?.extend = 'this is extend'
   }
 
 }
