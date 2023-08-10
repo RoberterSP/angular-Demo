761d6ad4120cd725c0e70212cb177a8552d6a59a
csp <shaopeng.chen@mcttechnology.com>
2023-08-10 15:21:05 +0800

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit  } from '@angular/core';
 import { globalInstanceSetting } from './share/globalInstance';
-
+import config from "devextreme/core/config";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +9,9 @@ import { globalInstanceSetting } from './share/globalInstance';
 export class AppComponent implements OnInit {
   title = 'angularDemo';
   ngOnInit() {
+    config({
+      defaultUseCurrencyAccountingStyle: true 
+    })
     globalInstanceSetting()
   }
 }
--- src/app/home/demo/datagrid/datagrid.component.ts
+++ src/app/home/demo/datagrid/datagrid.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Service } from '../service/app.service';
-
+import { Workbook } from 'exceljs';
+import * as saveAs from 'file-saver';
+// Our demo infrastructure requires us to use 'file-saver-es'. We recommend that you use the official 'file-saver' package in your applications.
+import { exportDataGrid } from 'devextreme/excel_exporter';
 @Component({
   selector: 'app-datagrid',
   templateUrl: './datagrid.component.html',
@@ -14,5 +17,19 @@ export class DemoDatagridComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onExporting(e) {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('export-supper');
 
+    exportDataGrid({
+      component: e.component,
+      worksheet,
+      autoFilterEnabled: true,
+    }).then(() => {
+      workbook.xlsx.writeBuffer().then((buffer) => {
+        saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'export-supper-data-grid.xlsx');
+      });
+    });
+    e.cancel = true;
+  }
 }
